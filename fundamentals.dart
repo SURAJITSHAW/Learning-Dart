7594d72d13b93fa8ddf668aba583e7adf240035f
Surajit <piklu3043@gmail.com>
2022-05-16 17:23:18 +0530

    feat: put some extra info about Dart 3

--- fundamentals.dart
+++ fundamentals.dart
@@ -1,3 +1,5 @@
+/* Dart is Object Oriented Prgramming Language, and here EVERYTHING is an Object */
+
 // Dart is a 1.Static Type (types of variables's are explicitly declared) & 2.Compiled (converted into Machine code) Programming Language
 
 // Dart supports 2 types of compilation - 1.AOT(for developemnet) & 2.JIT(for deployment or at delivery time)
@@ -5,7 +7,7 @@
 // Every Dart program starts with a main function
 // "import 'dart:core';" -> this package contains all the built-in functionality, also this package by default automatically imported. We don't had to do it explicitly
 void main() {
-  // Variable declared with this 'var' keyword behaved like a dyanamically typed variable cuz you could any type of value in this
+  // Variable declared with this 'var' keyword behaved can chamged the value later in the code but can't change the Type.
   var firstName = "Surajit";
   String lastName = "Shaw";
 
