79904a20e51b776ee05675085e74e426eabd1f2a
Surajit <piklu3043@gmail.com>
2022-05-16 17:45:47 +0530

    feat: 1.String Interpolation 2. Multi-line Strings

--- strings.dart
+++ strings.dart
@@ -7,9 +7,23 @@ main() {
   print(s1);
   print(s2);
   print(s3);
-  print(s4);
+  print(s4);  
 
   // RAW STRING (just put a 'r' infront of the string)
   String rawString = r"Inside raw string even \n  doesn't get any special treatment";
   print(rawString);
+
+  /* String Interpolation --> Replace the value of a variable within a string */
+  String name = 'Surajit';
+  String complement = '$name is a very good boy';
+  print(complement);
+
+  /* Multi-line String --> using 3 single or double quotes */
+  String mString1 = '''Let's play,
+  Football today.''';
+  String mString2 = """Hey,
+  How are you been ?""";
+
+  print(mString1);
+  print(mString2);
 }
