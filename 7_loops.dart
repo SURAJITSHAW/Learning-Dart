27eaa63389befcba0692f75d809775015f4745be
Surajit <piklu3043@gmail.com>
2022-05-18 12:04:54 +0530

    feat: complete the loops in Dart

---
+++ 7_loops.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var numbers = [1,2,3,4];
+  // 1. Standard for loop
+  // 2. for...in loop
+  for (var num in numbers) {
+
+    print(num);
+  }
+  // 3. forEach() --> Baiscally a higher order function
+  numbers.forEach((element) => {print(element)});
+  // 4. while loop
+  // 5. do...while loop
+}
