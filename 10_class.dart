3d9ca8f308a08975264d1158fd89256672424465
Surajit <piklu3043@gmail.com>
2022-05-19 22:26:10 +0530

    feat: diff kind of const variables in a dart class

--- 10_class.dart
+++ 10_class.dart
@@ -3,15 +3,16 @@ class Person {
   late int age;
 
   // constructor/default constructor
-  Person(String name, [int age = 18]) {
+  Person(String name, this.firstName, [int age = 18]) {
     this.name = name;
     this.age = age;
   }
 
-  // shortcut: Person(this.name, [this.age=18])
+  // shortcut: Person(this.name, [this.age=18]) 
+  // If you don't have anything to do with in a Constructor rather than assigning values to the properteis. You could skip the curly braces part, like above.
 
   /* Named Constructor */
-  Person.guest() {
+  Person.guest(this.firstName) {
     name='Guest';
     age=20;
   }
@@ -19,10 +20,21 @@ class Person {
   void showOutput() {
     print(name);
     print(age);
+    print(firstName);
   }
+
+  /* Constant Values in Class */
+  final firstName; // type will be defined by inferred value. You can assign them with any constructor at runtime, but can't reassign it further. Run time constant.
+  static const int myAge = 19; // But in case 'const' you can't change the value at run time or ever. Compile time constant.
+
+  Person.constVar(this.firstName);
+
+
+
+
 }
 
 main() {
-  Person person1 = Person('shaw');
+  Person person1 = Person('surajit shaw', 'surajit');
   person1.showOutput();
 }
